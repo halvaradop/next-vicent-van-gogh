1e80e171dabc4f3e0dc6dad1017f02eeee512dc3
halvaradop <hernanvid123@gmail.com>
2024-09-03 23:18:39 -0500

    chore: set up tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,19 @@
 import type { Config } from "tailwindcss"
+import utilities from "@halvaradop/tailwindcss-utilities"
 
 const config: Config = {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				metrophobic: "var(--font-metrophobic)",
+			},
+		},
 	},
-	plugins: [],
+	plugins: [utilities],
 }
 export default config
