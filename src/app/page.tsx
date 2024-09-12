36ae90f525cce14ecfca833bbf401f197cf66f79
Hernan Alvarado <hernanvid123@gmail.com>
2024-09-12 11:34:17 -0500

    feat: add index section page (#3)
    * feat: add index section page
    
    * chore: update ui components
    
    * chore: install framer-motion
    
    * chore: improve hero and artistic-style componentse
    
    * chore: add classes for horizontal scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-const Index = () => {}
+import { Hero } from "@/ui/index/hero"
+import { ArtisticStyle } from "@/ui/index/artistic-style"
+
+const Index = () => {
+	return (
+		<main className="flex flex-row items-center fluency-2xl text-center overflow-y-hidden">
+			<Hero />
+			<ArtisticStyle />
+		</main>
+	)
+}
 
 export default Index
