26a67e6d8ab0f88d67ce8dbb3722c139b8393182
Hernan Alvarado <hernanvid123@gmail.com>
2024-11-07 23:25:56 +0000

    chore: run `format` (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import { Hero } from "@/ui/index/hero"
 import { ArtisticStyle } from "@/ui/index/artistic-style"
 
 const Index = () => {
-	return (
-		<main className="flex flex-row items-center fluency-2xl text-center overflow-y-hidden">
-			<Hero />
-			<ArtisticStyle />
-		</main>
-	)
+    return (
+        <main className="flex flex-row items-center fluency-2xl text-center overflow-y-hidden">
+            <Hero />
+            <ArtisticStyle />
+        </main>
+    )
 }
 
 export default Index
