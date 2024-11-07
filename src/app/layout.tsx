2684dcb134feafdbbd2842169483eecc01ea0a62
Hernan Alvarado <hernanvid123@gmail.com>
2024-11-07 18:23:24 -0500

    chore: run `format`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,46 +4,46 @@ import { LayoutProps } from "@/lib/@types/props"
 import "@/ui/globals.css"
 
 const inter = Metrophobic({
-	weight: "400",
-	subsets: ["latin"],
-	variable: "--font-metrophobic",
+    weight: "400",
+    subsets: ["latin"],
+    variable: "--font-metrophobic",
 })
 
 const title = "Vicent Van Gogh Museum"
 const description = "Virtual Museum of Vicent Van Gogh, a little about his life and his works"
 
 export const metadata: Metadata = {
-	title,
-	description,
-	category: "art",
-	applicationName: title,
-	robots: "index, follow",
-	classification: "website",
-	keywords: "art, museum, vicent van gogh, paintings, history, virtual museum",
-	creator: "halvaradop <hernanvid123@gmail.com>",
-	authors: {
-		name: "halvaradop <hernanvid123@gmail.com>",
-	},
-	openGraph: {
-		title,
-		description,
-		siteName: title,
-		type: "website",
-		countryName: "Colombia",
-		alternateLocale: "en_CO",
-		locale: "en_CO",
-	},
-	twitter: {
-		title,
-		description,
-		creator: "halvaradop",
-	},
+    title,
+    description,
+    category: "art",
+    applicationName: title,
+    robots: "index, follow",
+    classification: "website",
+    keywords: "art, museum, vicent van gogh, paintings, history, virtual museum",
+    creator: "halvaradop <hernanvid123@gmail.com>",
+    authors: {
+        name: "halvaradop <hernanvid123@gmail.com>",
+    },
+    openGraph: {
+        title,
+        description,
+        siteName: title,
+        type: "website",
+        countryName: "Colombia",
+        alternateLocale: "en_CO",
+        locale: "en_CO",
+    },
+    twitter: {
+        title,
+        description,
+        creator: "halvaradop",
+    },
 }
 
 export default function RootLayout({ children }: LayoutProps) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	)
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,17 @@ import type { Config } from "tailwindcss"
 import utilities from "@halvaradop/tailwindcss-utilities"
 
 const config: Config = {
-	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/ui/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-	theme: {
-		extend: {
-			screens: {
-				base: "900px",
-			},
-			fontFamily: {
-				metrophobic: "var(--font-metrophobic)",
-			},
-		},
-	},
-	plugins: [utilities],
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/ui/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            screens: {
+                base: "900px",
+            },
+            fontFamily: {
+                metrophobic: "var(--font-metrophobic)",
+            },
+        },
+    },
+    plugins: [utilities],
 }
 export default config
